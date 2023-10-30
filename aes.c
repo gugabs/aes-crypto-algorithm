b1771306a52004fcd08452ce99f53158b317d802
Guga <contato.gustavobarbosa@outlook.com>
2023-10-30 05:30:53 -0300

    Fix S-Box initialization identation

--- aes.c
+++ aes.c
@@ -116,27 +116,27 @@ void read_line(FILE* credentials, BYTE** target) {
 // Wikipedia AES S-Box Implementation
 
 void initialize_aes_sbox(uint8_t sbox[256]) {
-	uint8_t p = 1, q = 1;
-	
-	/* loop invariant: p * q == 1 in the Galois field */
-	do {
-		/* multiply p by 3 */
-		p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+  uint8_t p = 1, q = 1;
 
-		/* divide q by 3 (equals multiplication by 0xf6) */
-		q ^= q << 1;
-		q ^= q << 2;
-		q ^= q << 4;
-		q ^= q & 0x80 ? 0x09 : 0;
+  /* loop invariant: p * q == 1 in the Galois field */
+  do {
+    /* multiply p by 3 */
+    p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
 
-		/* compute the affine transformation */
-		uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+    /* divide q by 3 (equals multiplication by 0xf6) */
+    q ^= q << 1;
+    q ^= q << 2;
+    q ^= q << 4;
+    q ^= q & 0x80 ? 0x09 : 0;
 
-		sbox[p] = xformed ^ 0x63;
-	} while (p != 1);
+    /* compute the affine transformation */
+    uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
 
-	/* 0 is a special case since it has no inverse */
-	sbox[0] = 0x63;
+    sbox[p] = xformed ^ 0x63;
+  } while (p != 1);
+
+  /* 0 is a special case since it has no inverse */
+  sbox[0] = 0x63;
 }
 
 void create_block(BYTE** block, BYTE* key) {
